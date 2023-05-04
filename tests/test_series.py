6067016418825fd9eb6aa484c877a038026c6ec8
almothana-almasri <mothana.almasri.99@gmail.com>
2023-05-04 13:18:59 +0000

    feat: test lucas

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,5 @@
 import pytest
-from Lab02.series import fibonacci
+from Lab02.series import fibonacci, lucas
 
 def test_fibonacci():
     assert fibonacci(0) == 0
@@ -9,3 +9,10 @@ def test_fibonacci():
     assert fibonacci(4) == 3
     assert fibonacci(5) == 5
 
+def test_lucas():
+    assert lucas(0) == 2
+    assert lucas(1) == 1
+    assert lucas(2) == 3
+    assert lucas(3) == 4
+    assert lucas(4) == 7
+    assert lucas(5) == 11
